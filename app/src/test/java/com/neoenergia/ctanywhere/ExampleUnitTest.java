f817158e8e5529eed20d3bff6dee8d5f20d566d1
Bruno <rodrigues.brodrigues@gmail.com>
2017-03-29 02:38:10 -0300

    Novas funcionalidades

--- app/src/test/java/com/neoenergia/ctanywhere/ExampleUnitTest.java
+++ app/src/test/java/com/neoenergia/ctanywhere/ExampleUnitTest.java
@@ -1,8 +1,11 @@
 package com.neoenergia.ctanywhere;
 
+import android.telephony.PhoneNumberUtils;
+
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -13,6 +16,8 @@ public class ExampleUnitTest {
 
     @Test
     public void addition_isCorrect() throws Exception {
+    
+        assertTrue(PhoneNumberUtils.isEmergencyNumber("190"));
         assertEquals(4, 2 + 2);
     }
 }
