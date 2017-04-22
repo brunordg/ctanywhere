ed7b91f31cbb76b48267538e958fc86498f739ad
Bruno <rodrigues.brodrigues@gmail.com>
2017-04-22 04:32:55 -0300

    Correção das versões

--- app/src/main/java/br/com/codeteam/ctanywhere/commons/BaseActivity.java
+++ app/src/main/java/br/com/codeteam/ctanywhere/commons/BaseActivity.java
@@ -1,5 +1,6 @@
 package br.com.codeteam.ctanywhere.commons;
 
+import android.app.ProgressDialog;
 import android.support.v7.app.AppCompatActivity;
 
 /**
@@ -8,4 +9,32 @@ import android.support.v7.app.AppCompatActivity;
 
 public abstract class BaseActivity extends AppCompatActivity {
     
+    public ProgressDialog mProgressDialog;
+    
+    /**
+     * Show ProgressDialog
+     */
+    public void showProgressDialog() {
+        if (this.mProgressDialog != null) {
+            this.mProgressDialog = new ProgressDialog(this);
+            this.mProgressDialog.setIndeterminate(true);
+        }
+        
+        this.mProgressDialog.show();
+    }
+    
+    /**
+     * Hide ProgressDialog
+     */
+    public void hideProgressDialog() {
+        if (this.mProgressDialog != null && this.mProgressDialog.isShowing()) {
+            this.mProgressDialog.dismiss();
+        }
+    }
+    
+    @Override
+    protected void onStop() {
+        super.onStop();
+        this.hideProgressDialog();
+    }
 }
