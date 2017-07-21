805786cee4d6ef28570742f85f19dd62e7710603
Bruno Rodrigues <brunorodrigues.rodrigues@portoseguro.com.br>
2017-07-21 17:32:43 -0300

    Inclusão da biblioteca Hawk

--- app/src/main/java/br/com/codeteam/ctanywhere/context/ApplicationContext.kt
+++ app/src/main/java/br/com/codeteam/ctanywhere/context/ApplicationContext.kt
@@ -1,9 +1,5 @@
 package br.com.codeteam.ctanywhere.context
 
 import android.app.Application
-import br.com.codeteam.ctanywhere.log.Log
-import br.com.codeteam.ctanywhere.preferences.Pref
-import br.com.codeteam.ctanywhere.preferences.PrefCrypto
-import br.ctanywhere.BuildConfig
 
 abstract class ApplicationContext : Application()
