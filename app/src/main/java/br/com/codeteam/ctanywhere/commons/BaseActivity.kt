a2dc5ee0010c5d3f875fe7246ef66eb651716357
Bruno Rodrigues <brunorodrigues.rodrigues@portoseguro.com.br>
2017-08-28 11:38:12 -0300

    Alterações no BaseActivity e criação do BaseView

--- app/src/main/java/br/com/codeteam/ctanywhere/commons/BaseActivity.kt
+++ app/src/main/java/br/com/codeteam/ctanywhere/commons/BaseActivity.kt
@@ -1,10 +1,15 @@
 package br.com.codeteam.ctanywhere.commons
 
+import android.content.Context
 import android.support.v7.app.AppCompatActivity
 import android.view.View
 import android.widget.ProgressBar
 
 abstract class BaseActivity : AppCompatActivity() {
+
+    fun getContext() : Context {
+        return applicationContext
+    }
 
     fun label(): String {
         return resources.getString(packageManager.getActivityInfo(componentName, 0).labelRes)
---
+++ app/src/main/java/br/com/codeteam/ctanywhere/commons/BaseView.kt
@@ -0,0 +1,7 @@
+package br.com.codeteam.ctanywhere.commons
+
+import android.content.Context
+
+interface BaseView {
+    fun getContext(): Context
+}
