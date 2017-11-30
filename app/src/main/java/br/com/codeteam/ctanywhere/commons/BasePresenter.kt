638c22a1f6a394a08b44ce6ad947b722b34a2422
Bruno Rodrigues <brunorodrigues.rodrigues@portoseguro.com.br>
2017-11-30 20:13:22 -0200

    Alteração no script do Travis

--- app/src/main/java/br/com/codeteam/ctanywhere/commons/BasePresenter.kt
+++ app/src/main/java/br/com/codeteam/ctanywhere/commons/BasePresenter.kt
@@ -5,7 +5,7 @@ import br.com.codeteam.ctanywhere.exception.CTRuntimeException
 
 abstract class BasePresenter<View : BaseView>: BaseInterfacePresenter<View> {
 
-    lateinit var view: View
+    private var view: View? = null
 
     override fun attach(view: View) {
         this.view = view
@@ -14,19 +14,13 @@ abstract class BasePresenter<View : BaseView>: BaseInterfacePresenter<View> {
     override fun view(): View {
         this.isViewValid()
 
-        return this.view
+        return this.view!!
     }
 
-    fun getContext(): Context? {
+    fun getContext(): Context {
         this.isViewValid()
 
-        if (this.view is BaseActivity || this.view is BaseFragment) {
-            //if (this.view.getContext() == null) throw CTRuntimeException("Context is null on: " + this.view.javaClass?.simpleName)
-
-            return this.view.getContext()
-        }
-
-        return null
+        return this.view?.getContext()!!
     }
 
     private fun isViewValid(): Boolean {
