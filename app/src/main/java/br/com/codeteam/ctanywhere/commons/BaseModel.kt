7aee507bf1e56f2aa906d651779ffd75eaaad46f
Bruno Rodrigues <brunorodrigues.rodrigues@portoseguro.com.br>
2018-04-20 15:18:07 -0300

    Ext for activity

--- app/src/main/java/br/com/codeteam/ctanywhere/commons/BaseModel.kt
+++ app/src/main/java/br/com/codeteam/ctanywhere/commons/BaseModel.kt
@@ -1,10 +1,15 @@
 package br.com.codeteam.ctanywhere.commons
 
 import br.com.codeteam.ctanywhere.utils.Json
+import org.json.JSONObject
 
 abstract class BaseModel {
 
     fun toJson(): String {
         return Json.toJson(this)
+    }
+
+    fun toJsonObject(): JSONObject {
+        return JSONObject(toJson())
     }
 }
