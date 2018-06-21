ea3e6d1ea0e9b6037a5c9287d2897181d83f73e5
Bruno Rodrigues <brunorodrigues.rodrigues@portoseguro.com.br>
2018-06-21 17:13:39 -0300

    Android Vector Drawables on Pre Lollipop Crash - Solution

--- app/src/main/java/br/com/codeteam/ctanywhere/view/webview/WebViewClient.kt
+++ app/src/main/java/br/com/codeteam/ctanywhere/view/webview/WebViewClient.kt
@@ -1,14 +1,35 @@
 package br.com.codeteam.ctanywhere.view.webview
 
 import android.annotation.TargetApi
+import android.graphics.Bitmap
 import android.os.Build
+import android.os.Handler
 import android.webkit.WebResourceRequest
 import android.webkit.WebView
 import android.webkit.WebViewClient
 
-abstract class WebViewClient: WebViewClient() {
+abstract class WebViewClient(private var timeout: Long = 30000): WebViewClient() {
+
+    private var isTimeout = true
 
     abstract fun handleUri(url: String): Boolean
+
+    abstract fun handleTimeout()
+
+    override fun onPageStarted(view: WebView?, url: String?, favicon: Bitmap?) {
+        super.onPageStarted(view, url, favicon)
+
+        Handler().postDelayed({
+            if (isTimeout) {
+                handleTimeout()
+            }
+        }, timeout)
+    }
+
+    override fun onPageFinished(view: WebView?, url: String?) {
+        super.onPageFinished(view, url)
+        isTimeout = false
+    }
 
     @TargetApi(Build.VERSION_CODES.LOLLIPOP)
     override fun shouldOverrideUrlLoading(view: WebView?, request: WebResourceRequest?): Boolean {
